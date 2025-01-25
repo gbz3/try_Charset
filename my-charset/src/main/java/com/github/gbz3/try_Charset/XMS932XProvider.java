a57936d5824f577c792798d89cc600ce8afbf8c3
gbz3 <1808483+gbz3@users.noreply.github.com>
2025-01-25 09:16:20 +0900

    Level 0

---
+++ my-charset/src/main/java/com/github/gbz3/try_Charset/XMS932XProvider.java
@@ -0,0 +1,20 @@
+package com.github.gbz3.try_Charset;
+
+import java.nio.charset.Charset;
+import java.nio.charset.spi.CharsetProvider;
+import java.util.Iterator;
+import java.util.List;
+
+public class XMS932XProvider extends CharsetProvider {
+    private final List<Charset> charsets = List.of(Charset.forName("MS932"));
+
+    @Override
+    public Iterator<Charset> charsets() {
+        return charsets.iterator();
+    }
+
+    @Override
+    public Charset charsetForName(String s) {
+        return "XMS932X".equals(s) ? charsets.get(0) : null;
+    }
+}
